a7cfc70310d97e72432ee0305cdd09d599b40c14
Tiago Luna <tiagodeluna@hotmail.com>
2017-03-13 22:48:15 -0300

    Capturing SMS messages

---
+++ app/src/main/java/br/com/tiagoluna/agenda/receiver/SMSReceiver.java
@@ -0,0 +1,18 @@
+package br.com.tiagoluna.agenda.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by tiago on 13/03/2017.
+ */
+
+public class SMSReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "SMS received!", Toast.LENGTH_SHORT).show();
+    }
+}
